e1c11021707f1b120db6f87a68a9ef13d1bf8034
rajapradeep <7948650+praddevops@users.noreply.github.com>
2020-05-19 22:14:22 -0500

    Update find_ips.py

--- find_ips.py
+++ find_ips.py
@@ -1,18 +1,28 @@
 """
-This python script finds all files in directory ip_files located at same level as this script file and 
+This python script finds all files in ip_files directory and its sub directories.
+ip_files directory must be located at same level as this script file and
 prints in sorted order all valid ip addresses
 """
 from pathlib import Path
 import re
 
 ip_candidates = []
+text_content = ""
 
-entries = Path('ip_files/')
-for entry in entries.iterdir():
-    if entry.is_file():
-        with open(entry) as f:
-            textcontent = f.read()
-            ip_candidates = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", textcontent)
+def find_ip_in_text(text):
+    return re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", text)
+
+def find_all_ip_in_dir(dir_name):
+    entries = Path(dir_name)
+    for entry in entries.iterdir():
+       if entry.is_file():
+           with open(entry) as f:
+              textcontent = f.read()
+              ip_candidates.extend(find_ip_in_text(textcontent))
+       elif entry.is_dir():
+           find_all_ip_in_dir(entry)
+
+find_all_ip_in_dir('ip_files')
 
 for ip in sorted(ip_candidates):
     print(ip)
